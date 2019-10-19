34180066c93787d0ccceacf343d4710509490df5
sudhanshusharma99 <56498080+sudhanshusharma99@users.noreply.github.com>
2019-10-19 15:55:56 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -80,3 +80,20 @@ int main()
     }
     return 0;
 }
+ int n, i, factorial;
+    factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;             
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
+}
