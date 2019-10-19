a89cd47521b63f43de8ea96f721612b273e99748
sudhanshusharma99 <56498080+sudhanshusharma99@users.noreply.github.com>
2019-10-19 15:51:15 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -60,4 +60,23 @@ return 0;
     printf("Reversed Number = %d", reverse);
     return 0;
 }
-
+#include <stdio.h>
+int main()
+{
+    int n, i, factorial;
+    factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;             
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
+}
